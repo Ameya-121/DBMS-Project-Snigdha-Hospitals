a0043c1531e967df21a203e2f6c3040dbc528e5e
neshvig10 <svigneshwaran1073@gmail.com>
2023-09-18 21:31:54 +0530

    first commit

---
+++ src/main/java/com/snigdha/snigdhahospitals/SnigdhahospitalsApplication.java
@@ -0,0 +1,13 @@
+package com.snigdha.snigdhahospitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnigdhahospitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnigdhahospitalsApplication.class, args);
+	}
+
+}
