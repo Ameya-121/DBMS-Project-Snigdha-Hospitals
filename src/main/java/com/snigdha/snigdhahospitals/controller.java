d71c31f88a199ebd4c6b50d0cd5a943c27a58b5a
neshvig10 <svigneshwaran1073@gmail.com>
2023-09-19 10:58:33 +0530

    links added

--- src/main/java/com/snigdha/snigdhahospitals/controller.java
+++ src/main/java/com/snigdha/snigdhahospitals/controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ModelAndView;
 
+import ch.qos.logback.core.model.Model;
+
 @RestController
 public class controller {
     
@@ -28,6 +30,44 @@ public class controller {
         mv.setViewName("doctors");
         return mv;
     }
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login");
+        return mv;
+    }
+
+    @RequestMapping("/signup")
+    public ModelAndView signup(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("signup");
+        return mv;
+    }
+
+    @RequestMapping("/appointments")
+    public ModelAndView appointments(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("appointments");
+        return mv;
+    }
+
+    @RequestMapping("/contactus")
+    public ModelAndView contactus(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("contactus");
+        return mv;
+    }
+
+    @RequestMapping("/grievance")
+    public ModelAndView grievance(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("grievance");
+        return mv;
+    }
+
+    
+
 
     
     
