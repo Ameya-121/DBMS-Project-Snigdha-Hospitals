4897d308a82a1da377965b5fd6104648901c5ea9
neshvig10 <svigneshwaran1073@gmail.com>
2023-09-20 22:20:21 +0530

    jdbc added

--- src/main/java/com/snigdha/snigdhahospitals/controller.java
+++ src/main/java/com/snigdha/snigdhahospitals/controller.java
@@ -1,19 +1,37 @@
 package com.snigdha.snigdhahospitals;
 
-// import org.springframework.web.bind.annotation.GetMapping;
+// import javax.swing.tree.RowMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.servlet.ModelAndView;
-// import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ModelAndView;
+// import org.springframework.web.servlet.ModelAndView;
 
 // import ch.qos.logback.core.model.Model;
 
 @RestController
 public class controller {
+
+
+    // @Autowired
+    // JdbcTemplate jdbcTemplate;
+
+    // @GetMapping("/d")
+    // public List<String> doctordata(){
+        
+    // }
+    
     
 
     @RequestMapping("/")
@@ -23,6 +41,12 @@ public class controller {
         return mv;
     }
 
+    @RequestMapping("/home")
+    public ModelAndView homepage(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home");
+        return mv;
+    }
 
     @RequestMapping("/doctors")
     public ModelAndView doctors(){
@@ -63,6 +87,13 @@ public class controller {
     public ModelAndView grievance(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("grievance");
+        return mv;
+    }
+
+    @RequestMapping("/aboutus")
+    public ModelAndView aboutus(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("aboutus");
         return mv;
     }
 
